94e73177f9cb5d2770862805897d019d441ec167
rajeshoo7 <rajeshsai.kammari@gmail.com>
2020-06-21 19:41:31 -0500

    ommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  todoArray = []
+
+  addTodo(value:any) {
+
+    this.todoArray.push(value)
+    console.log(this.todoArray[this.todoArray.length-1])
+
+  }
+/*delete item*/  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) { this.todoArray.splice(i, 1) }
+    }
+  }
+
+
+  // submit Form 
+   todoSubmit(value:any)
+   { console.log(value)
+    }
+
+
+
 }
+
